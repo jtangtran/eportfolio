7faa00b8a95d2b742fa79783322e15bc1051f4e5
Jennifer Tang-Tran <phuongtangtran@gmail.com>
2021-03-16 00:16:21 -0700

    Updates

---
+++ precache-manifest.d3f4e1a5e322ed98ca15483f02e37e0c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07775800d3c34227e09fc686af86059",
+    "url": "/%7Brepository%7D/index.html"
+  },
+  {
+    "revision": "ff9ac1e40ac3f873ff11",
+    "url": "/%7Brepository%7D/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "fc8e165965253a2c4948",
+    "url": "/%7Brepository%7D/static/css/main.7dad816f.chunk.css"
+  },
+  {
+    "revision": "ff9ac1e40ac3f873ff11",
+    "url": "/%7Brepository%7D/static/js/2.77bad8e5.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/%7Brepository%7D/static/js/2.77bad8e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc8e165965253a2c4948",
+    "url": "/%7Brepository%7D/static/js/main.94d49772.chunk.js"
+  },
+  {
+    "revision": "b200f461d2a396f07a7f",
+    "url": "/%7Brepository%7D/static/js/runtime-main.f6e08e33.js"
+  },
+  {
+    "revision": "a549aa36fd310083be0f833f4393dfa7",
+    "url": "/%7Brepository%7D/static/media/ecom.a549aa36.jpg"
+  },
+  {
+    "revision": "a9e55691091fbf5140de663e1129f705",
+    "url": "/%7Brepository%7D/static/media/mlc.a9e55691.png"
+  },
+  {
+    "revision": "1551a0dceaf403300f7ce24524b6b7c8",
+    "url": "/%7Brepository%7D/static/media/myProfile.1551a0dc.jpeg"
+  }
+]);
